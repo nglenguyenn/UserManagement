70ae92081fe518e9e8036112ba3c85c04373eace
Nguyen Le Nguyen <82690801+nglenguyenn@users.noreply.github.com>
2023-07-29 09:38:47 +0700

    Add Controller

---
+++ UserManagement.Api/Controllers/UserListController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.Api.Controllers
+{
+    [Route("api/userlist")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class UserListController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var uerlist = await Task.FromResult(new string[] { "Virat", "Messi", "Ozil", "Lara", "MS Dhoni" });
+            return Ok(uerlist);
+        }
+    }
+}
